3861d224280e6c9efd75fb9c33b98ecd14dd6aa2
Ivan Huang <yanziily@gmail.com>
2022-05-20 16:24:11 +0800

    fix typo

--- build_on_mac.sh
+++ build_on_mac.sh
@@ -7,4 +7,4 @@ docker build -t android-build-box:focal \
         --build-arg GIT_NAME="echo $(git config --get user.name)" \
         --build-arg GIT_EMAIL="echo $(git config --get user.email)" \
         --build-arg ROOT_PASSWD="12345678" \
-		.
+        .
